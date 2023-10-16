ec1f2de336172100c8752acd083bba9d4a2cf62b
Kuah_0 <gdsygrt557422@naver.com>
2023-10-16 19:58:17 +0900

    refactor: Role(key, title) 추가 및 Provider(LOCAL) 추가

--- src/main/java/whatseating/backend/domain/user/user/domain/enums/Provider.java
+++ src/main/java/whatseating/backend/domain/user/user/domain/enums/Provider.java
@@ -1,4 +1,4 @@
-package whatseating.backend.domain.user.domain.enums;
+package whatseating.backend.domain.user.user.domain.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Provider {
-    KAKAO, NAVER, GOOGLE
+    KAKAO, NAVER, GOOGLE, LOCAL
 }
---
+++ src/main/java/whatseating/backend/domain/user/user/domain/enums/Role.java
@@ -0,0 +1,14 @@
+package whatseating.backend.domain.user.user.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN("ROLE_ADMIN", "관리자"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
